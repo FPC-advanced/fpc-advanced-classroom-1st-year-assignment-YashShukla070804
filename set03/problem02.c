ca59ae3359e23b7c57e5c37f905886a3f0210f40
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-10-30 22:07:09 +0530

    committed 10/30/2023, 10:07:08 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
 int main() {
     float x1, y1, x2, y2, x3, y3;
     int result;
