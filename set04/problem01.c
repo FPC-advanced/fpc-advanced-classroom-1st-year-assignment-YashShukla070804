a67f786188fb76636ba6fbd1c8afcb79bc69f3a9
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-10-31 11:21:02 +0530

    committed 10/31/2023, 11:21:01 AM

--- set04/problem01.c
+++ set04/problem01.c
@@ -2,7 +2,6 @@
 void input(int *num1, int *den1, int *num2, int *den2);
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
-```
 int main() {
     int num1, den1, num2, den2, res_num, res_den;
 
