ee52d277a653efc3f5e9fd525b7525de51124ab6
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-11-19 22:56:21 +0530

    committed 11/19/2023, 10:56:20 PM

--- set04/problem06.c
+++ set04/problem06.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to input a string
 void input_string(char *a) {
     printf("Enter a string: ");
-    fgets(a, 1000, stdin); // Using fgets to allow spaces in the input
+    fgets(a, 1000, stdin);
 }
 
-// Function to count the number of words in a string
 int count_words(char *string) {
     char *token;
     int count = 0;
 
-    // Using strtok to tokenize the string based on space and count the words
     token = strtok(string, " ");
     while (token != NULL) {
         count++;
@@ -22,7 +19,6 @@ int count_words(char *string) {
     return count;
 }
 
-// Function to output the result
 void output(char *string, int no_words) {
     printf("The string is: %s\n", string);
     printf("Number of words in the string: %d\n", no_words);
