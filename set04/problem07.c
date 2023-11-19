ccf452e90a7340c1496becec174bcdc91b4c8f30
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-11-19 23:07:50 +0530

    committed 11/19/2023, 11:07:49 PM

--- set04/problem07.c
+++ set04/problem07.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
+
 typedef struct {
     int num, den;
 } Fraction;
 
-Fraction input_fraction();
-int find_gcd(int a, int b);
-Fraction add_fractions(Fraction f1, Fraction f2);
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum);
-
-int main() {
-    Fraction frac1 = input_fraction();
-    Fraction frac2 = input_fraction();
-
-    Fraction sum = add_fractions(frac1, frac2);
-
-    output(frac1, frac2, sum);
-
-    return 0;
-}
-
+// Function to input a fraction
 Fraction input_fraction() {
     Fraction frac;
     printf("Enter the numerator: ");
@@ -30,6 +16,7 @@ Fraction input_fraction() {
     return frac;
 }
 
+// Function to find the greatest common divisor (GCD) of two numbers
 int find_gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -39,13 +26,17 @@ int find_gcd(int a, int b) {
     return a;
 }
 
+// Function to add two fractions
 Fraction add_fractions(Fraction f1, Fraction f2) {
     Fraction sum;
 
+    // Cross-multiply to find a common denominator
     sum.den = f1.den * f2.den;
 
+    // Add the numerators
     sum.num = f1.num * f2.den + f2.num * f1.den;
 
+    // Simplify the result by dividing numerator and denominator by their GCD
     int gcd = find_gcd(sum.num, sum.den);
     sum.num /= gcd;
     sum.den /= gcd;
@@ -57,4 +48,15 @@ void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum) {
     printf("Fraction 1: %d/%d\n", f1.num, f1.den);
     printf("Fraction 2: %d/%d\n", f2.num, f2.den);
     printf("The sum of fractions is: %d/%d\n", sum.num, sum.den);
+}
+
+int main() {
+    Fraction frac1 = input_fraction();
+    Fraction frac2 = input_fraction();
+
+    Fraction sum = add_fractions(frac1, frac2);
+
+    output(frac1, frac2, sum);
+
+    return 0;
 }
