99935ce6744aa77799e5c706c2c3f172a27ff36b
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-11-19 23:46:15 +0530

    committed 11/19/2023, 11:46:13 PM

--- set05/problem04.c
+++ set05/problem04.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+void input_camel_details(float *radius, float *height, float *length);
+int find_mood(float radius, float height, float length)
+void output(float radius, float height, float length, int mood);
+
 int main() {
     float stomach_radius, camel_height, camel_length;
     int camel_mood;
