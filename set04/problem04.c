cdce170fd1cf3d3443a27b68c539fd90d27fc07a
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-11-19 22:46:44 +0530

    committed 11/19/2023, 10:46:43 PM

--- set04/problem04.c
+++ set04/problem04.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+int input_degree();
+void input_coefficients(int n, float a[n]);
+float input_x();
+float evaluate_polynomial(int n, float a[n], float x);
+void output(int n, float a[n], float x, float result);
 int main() {
     int degree = input_degree();
 
