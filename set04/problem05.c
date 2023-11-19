b8331ee07823681a61abcc6b007550b2c50f9f06
Yash Shukla <yashs.bsc23@rvu.edu.in>
2023-11-19 22:53:41 +0530

    committed 11/19/2023, 10:53:39 PM

--- set04/problem05.c
+++ set04/problem05.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+int input_size();
+void input_array(int n, int a[n]);
+int find_largest_index(int n, int a[n]);
+void output(int index);
 int main() {
     int size = input_size();
 
